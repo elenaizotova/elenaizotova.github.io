0f48e15cd18f6b4aa7e0af81e59ec858d742280d
Elena Izotova <elena.k.izotova@gmail.com>
2019-05-15 13:26:03 -0400

    Added animation and MYR info.

---
+++ References/reference.js
@@ -0,0 +1,15 @@
+var red = [359, 100, 29.8];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+
+var teal = [180, 98, 17];
+
+var myName = "Elena Izotova";
+var letterColors = [teal];
+var bubbleShape = "circle";
+
+drawName(myName, letterColors);
+bounceName();
+bounceBubbles();
