34e9594b90572cfe8e1197f91e601cea6ed99a0f
Elena Izotova <elena.k.izotova@gmail.com>
2019-11-05 20:59:31 -0500

    ?

---
+++ build/precache-manifest.2a5b88c48d17d0006e035d6ca9c42c2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0659527c0de2ab73131e1c0322cda049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8f35ddf4faa1e9ab33d",
+    "url": "/static/css/main.9244bae7.chunk.css"
+  },
+  {
+    "revision": "3ce109401dd26c49a363",
+    "url": "/static/js/2.e6a6b5b4.chunk.js"
+  },
+  {
+    "revision": "c8f35ddf4faa1e9ab33d",
+    "url": "/static/js/main.3bdd3f4d.chunk.js"
+  },
+  {
+    "revision": "4469a5f78160f9b4d886",
+    "url": "/static/js/runtime-main.ee25f581.js"
+  },
+  {
+    "revision": "40165cbbc55b229769503efa326c8c1e",
+    "url": "/static/media/MYR_Chem.40165cbb.png"
+  }
+]);
