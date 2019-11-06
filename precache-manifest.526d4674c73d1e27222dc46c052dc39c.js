a2620d4f48af68ffc8f9dfb30c8fe8fe6011d03a
Elena Izotova <elena.k.izotova@gmail.com>
2019-11-05 20:31:04 -0500

    Updates

---
+++ precache-manifest.526d4674c73d1e27222dc46c052dc39c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b5aff857a1b067cda832a1c7673661",
+    "url": "elenaizotova.github.io/index.html"
+  },
+  {
+    "revision": "bd420df631398f0a1c15",
+    "url": "elenaizotova.github.io/static/css/main.9244bae7.chunk.css"
+  },
+  {
+    "revision": "3ce109401dd26c49a363",
+    "url": "elenaizotova.github.io/static/js/2.e6a6b5b4.chunk.js"
+  },
+  {
+    "revision": "bd420df631398f0a1c15",
+    "url": "elenaizotova.github.io/static/js/main.5ca1dd8e.chunk.js"
+  },
+  {
+    "revision": "73d11a81b1b93b744410",
+    "url": "elenaizotova.github.io/static/js/runtime-main.157a86fb.js"
+  },
+  {
+    "revision": "40165cbbc55b229769503efa326c8c1e",
+    "url": "elenaizotova.github.io/static/media/MYR_Chem.40165cbb.png"
+  }
+]);
